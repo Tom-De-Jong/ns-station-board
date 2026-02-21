91e3f0ac4fa9f458f5d85f1257e66cf12643824c
Tom-De-Jong <tomdejong2009@gmail.com>
2026-02-21 16:28:16 +0100

    added comments to javascript and changed a little

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let trainType = document.querySelector(".trainType");
 let destination = document.querySelector(".destination");
 let via = document.querySelector(".via");
 let nextTrain = document.querySelector(".nextTrain")
-let trainNumber = 1;
+let trainNumber = 0;
 
 
 fetch('https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures?uicCode=8400337&maxJourneys=10', {
@@ -23,21 +23,21 @@ fetch('https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures?uicC
         console.log(data.payload);
         console.log(data.payload.departures[trainNumber]);
 
+        // hier worden de variables van de aankomsttijd en de tijd van nu gemaakt en berekend in hoeveel minuten de eerste trein komt
         let trainTime = data.payload.departures[trainNumber].actualDateTime;
         let newDate = new Date(trainTime);
         let now = new Date();
-        // console.log(now.getMinutes());
-        // console.log(newDate.getMinutes());
-
         let waitingTime = newDate.getMinutes() - now.getMinutes()
-        console.log("waiting time: " + waitingTime);
+        console.log("waiting time: " + waitingTime + " minuten");
 
-        // als de trein via andere plekken gaat
+
+        // als de trein via andere plekken gaat laat hij het in de view zien doormiddel van deze if statements
         if (data.payload.departures[trainNumber].routeStations.length != 0) {
 
             console.log(data.payload.departures[trainNumber].routeStations);
             let viaStations = "via"
 
+            // deze for loop is er om te kijken of hij achter het station een komma moet zetten if niet
             for (let i = 0; i < data.payload.departures[trainNumber].routeStations.length; i++) {
 
                 if (i < data.payload.departures[trainNumber].routeStations.length - 1) {
@@ -49,20 +49,17 @@ fetch('https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures?uicC
                 }
 
             }
+            // als de trein stopt bij andere stations wordt het in de view gezet
             via.innerHTML = viaStations;
         }
 
+        // hier worden de variables voor de volgende trein gemaakt
         nextTrainDate = new Date(data.payload.departures[trainNumber + 1].actualDateTime)
         nextTrainTime = nextTrainDate.getHours() + ":" + nextTrainDate.getMinutes();
         nextTrainShort = data.payload.departures[trainNumber + 1].product.categoryCode
         nextTrainDirection = data.payload.departures[trainNumber + 1].direction
         
-        
-
-
-
-        let routeStationsConnected = "";
-
+        // volgende trein wordt in de view gezet
         minuteDisplay.innerHTML = waitingTime;
         trainType.innerHTML = data.payload.departures[trainNumber].product.longCategoryName;
         destination.innerHTML = data.payload.departures[trainNumber].direction
